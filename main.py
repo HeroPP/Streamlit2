0628f3be1bdb69f722d90e8eac50ae91dad5dac0
Jaap <jaap1@me.com>
2022-03-04 17:00:38 +0100

    req

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from teamleader import Client
 import streamlit as st
+from teamleaderApiV1.client import Client
+import pandas as pd
 
+data_load_state = st.text("haha")
+tl = Client(
+    api_group=st.secrets["teamleader_v1"]["api_group"],
+    api_secret=st.secrets["teamleader_v1"]["api_secret"],
+)
 
-data_load_state = st.text('haha')
-tl = Client()
+df = pd.DataFrame(tl.companies.list())
+st.write(df)
