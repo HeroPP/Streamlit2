93568283dc521da4458011dc37d1d995b064a1bd
Jaap <jaap1@me.com>
2022-03-18 15:32:23 +0100

    testtt

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import streamlit as st
 from teamleaderApiV1.client import Client
 import pandas as pd
+from teamleader import Client as TL2
+
+tl2 = TL2(
+    client_id=st.secrets["teamleader_v2"]["client_id"],
+    client_secret=st.secrets["teamleader_v2"]["client_secret"],
+    teamleader_token_file_name=st.secrets["teamleader_v2"][
+        "teamleader_token_file_name"
+    ],
+)
+df2 = pd.DataFrame(tl2.companies.list())
+st.write(df2)
 
 data_load_state = st.text("haha")
 tl = Client(
